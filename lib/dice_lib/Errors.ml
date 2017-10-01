6aa98f90b92d56a4c693cbf746a88f224823b1e7
Brian Pratt <brianpratt@fastmail.fm>
2017-10-01 12:19:20 -0500

    Removed errors of unused types

--- lib/dice_lib/Errors.ml
+++ lib/dice_lib/Errors.ml
@@ -4,18 +4,13 @@ module RollError = struct
     | DieOrPoolDoesntExist of string
 end
 
-(* type pool_error = *)
-(*   | RecursionFound of Pool.t * Pool.t *)
-
 module GameError = struct
   type t =
     | NoneLoaded
     (* | GameDoesNotExist of Game.t *)
     (* | DieDoesNotExist of Die.t *)
-    (* | PoolDoesNotExist of Pool.t *)
 end
 
 type t =
   | RollError of RollError.t
   | GameError of GameError.t
-(*   | Pool of pool_error *)
