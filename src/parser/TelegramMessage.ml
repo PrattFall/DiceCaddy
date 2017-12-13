eb3e858f2fb3832a930668342dc78fd63becc8a3
Brian Pratt <brianpratt@fastmail.fm>
2017-12-13 14:32:24 -0600

    Changes to fix bugs from testing

--- src/parser/TelegramMessage.ml
+++ src/parser/TelegramMessage.ml
@@ -4,36 +4,47 @@ open DiceLib
 open CCParse
 open CCParse.Infix
 
-type die_roll = DieRoll of int * string
-
 let slash = char '/'
 let comma = char ','
 let quote = char '"'
-
-let command s =
-  slash *> string s <* skip_white
 
 let trim p =
   skip_white *> p <* skip_white
 
 let full_string =
-  let quoted p = quote *> p <* quote in
+  let non_quote_string = chars1_if (fun c -> c != '"') in
+  let bind_trim s = pure (String.trim s) in
 
-  quoted (chars1_if (fun c -> c != '"'))
+  quote *> non_quote_string <* quote
+  >>= bind_trim
+
+let sub_command_failure command =
+  try_ (U.word >>=
+      (fun sub ->
+        fail (Format.sprintf
+                "No known sub-command '%s' for command '%s'."
+                sub
+                command)))
+
+let command s =
+  slash *> string s <* skip_white
+
+let sub_command s = trim (string s)
 
 (* Syntax: '"%s" %d %d, ...' -> die name, min value, max value *)
 module DieSideDescription = struct
   let range =
     trim U.int  >>=
     (fun min -> trim U.int >>=
-    (fun max -> return (Range.make min max)))
+    (fun max -> pure (Range.make min max)))
 
   let value =
     trim full_string >>=
-    (fun sideName -> trim range >>=
-    (fun range -> return (DieFaceValue.make sideName range)))
+    (fun valueName -> trim range >>=
+    (fun range -> pure (DieFaceValue.make valueName range)))
 
-  let parse = sep1 ~by:comma value
+  let parse =
+    try_ (sep1 ~by:comma value)
 end
 
 module DieCommand = struct
@@ -41,92 +52,139 @@ module DieCommand = struct
     | Create of string
     | Remove of string
     | AddSide of string * DieSide.t
-    | RemoveSide of int * string
+    | RemoveSide of string * int * int
     | Show of string
-
-  let die_command = command "die"
 
   (* Create a new die *)
   (* Syntax: '/die create "%s"' -> die name *)
   let create =
-    die_command *>
-    trim (string "create") *>
-    full_string >>=
-    (fun w -> return (Create w))
+    try_ (
+      sub_command "create" *>
+      full_string >>=
+      (fun w -> pure (Create w))
+    )
 
   (* Remove a die from the game by name *)
   (* Syntax: '/die remove "%s"' -> die name *)
   let remove =
-    die_command *>
-    trim (string "remove") *>
-    full_string >>=
-    (fun w -> return (Remove w))
+    try_ (
+      sub_command "remove" *>
+      full_string >>=
+      (fun w -> pure (Remove w))
+    )
 
   (* Show all sides of a die *)
   (* Syntax: '/die show "%s"' -> die name *)
   let show =
-    die_command *>
-    trim (string "show") *>
-    full_string >>=
-    (fun w -> return (Show w))
+    try_ (
+      sub_command "show" *>
+      full_string >>=
+      (fun w -> pure (Show w))
+    )
 
   (* Add a new side to a die *)
-  (* Syntax: '/die addside "%s" %d "%s" %d %d, ...', die name, number added,
-   *         value name, min value, max value
-   *)
+  (* Syntax: '/die addside "%s" %d die_value, ...', die name, number added *)
   let add_side =
-    die_command *>
-    trim (string "addside") *>
-    full_string >>=
-    (fun die_name -> trim U.int >>=
-    (fun numAdded -> trim DieSideDescription.parse >>=
-    (fun value -> return (AddSide (die_name, (DieSide.make numAdded value))))))
+    let make die_name numAdded value =
+      pure (AddSide (die_name, (DieSide.make numAdded value)))
+    in
+
+    try_ (
+      sub_command "addside" *>
+      full_string >>=
+      (fun die_name -> trim U.int >>=
+      (fun numAdded -> trim DieSideDescription.parse >>=
+      (fun value -> make die_name numAdded value)))
+    )
 
   (* Remove a side frmo a die by its id*)
-  (* Syntax: '/die removeside "%s" , die name, side id*)
+  (* Syntax: '/die removeside "%s" %d %d, die name, num to remove, side id*)
   let remove_side =
-    die_command *>
-    trim (string "removeside") *>
-    full_string >>=
-    (fun die_name -> U.int >>=
-    (fun side_id -> return (RemoveSide (side_id, die_name))))
+    let syntax =
+      full_string >>=
+      (fun die_name -> trim U.int >>=
+      (fun n -> trim U.int >>=
+      (fun side_id -> pure (RemoveSide (die_name, n, side_id)))))
+    in
+
+    try_ (sub_command "removeside" *> syntax)
+
+  (* Parse any of the above *)
+  let parse =
+    try_ (command "die") *>
+    (
+      create <|>
+      remove <|>
+      show <|>
+      add_side <|>
+      remove_side
+    )
 end
 
 module GameCommand = struct
   type t =
     | Create of string
     | Remove of string
-    | Start of string
     | Switch of string
+    | Start
+    | End
     | ListDice
 
-  let game_command = command "game"
-
-  let sub_command name =
-    game_command *>
-    trim (string name) *>
+  let game_command name =
+    sub_command name *>
     full_string
 
   let create =
-    sub_command "create" >>=
-    (fun w -> return (Create w))
+    try_ (
+      game_command "create" >>=
+      (fun w -> pure (Create w))
+    )
 
   let remove =
-    sub_command "remove" >>=
-    (fun w -> return (Remove w))
-
-  let start =
-    sub_command "start" >>=
-    (fun w -> return (Start w))
+    try_ (
+      game_command "remove" >>=
+      (fun w -> pure (Remove w))
+    )
 
   let switch =
-    sub_command "switch" >>=
-    (fun w -> return (Switch w))
+    try_ (
+      game_command "switch" >>=
+      (fun w -> pure (Switch w))
+    )
+
+  let start_game =
+    try_ (
+      sub_command "start" >>=
+      (fun w -> pure Start)
+    )
+
+  let end_game =
+    try_ (
+      sub_command "end" >>=
+      (fun w -> pure End)
+    )
 
   let list_dice =
-    string "listDice" *>
-    return ListDice
+    try_ (
+      sub_command "listdice" *>
+      pure ListDice
+    )
+
+  (* Parse any of the above *)
+  let parse =
+    try_ (command "game") *>
+    (
+      create <|>
+      remove <|>
+      switch <|>
+      start_game <|>
+      end_game <|>
+      list_dice <|>
+      sub_command_failure "game"
+    )
 end
+
+type die_roll = DieRoll of int * string
 
 type t =
   | Roll of die_roll list
@@ -137,17 +195,33 @@ let die =
   U.int <*
   skip_white >>=
   (fun n -> full_string >>=
-  (fun w -> return (DieRoll (n, w))))
+  (fun w -> pure (DieRoll (n, w))))
 
 let dice =
-  sep ~by:comma (trim die)
+  sep1 ~by:comma (trim die)
 
-let make_roll ds = return (Roll ds)
+let make_roll ds = pure (Roll ds)
 
 let roll_command =
-  command "roll" *>
+  try_ (command "roll") *>
   dice >>=
   make_roll
 
-let parse json_string =
-  "Test"
+let die_command =
+  DieCommand.parse >>= (fun x -> pure (Die x))
+
+let game_command =
+  GameCommand.parse >>= (fun x -> pure (Game x))
+
+let unknown_command =
+  try_ (slash *> U.word >>= (fun w -> failf "Command '%s' does not exist." w))
+
+let telegram_command =
+  roll_command <|>
+  die_command <|>
+  game_command <|>
+  unknown_command <?>
+  "Incorrect syntax"
+
+let parse s =
+  parse_string telegram_command s
