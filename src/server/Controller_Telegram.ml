fcdf601856725b866e988c9a23740e41b1ea11c8
Brian Pratt <brianpratt@fastmail.fm>
2017-12-13 14:34:02 -0600

    Changes to server to support new Telegram parser

--- src/server/Controller_Telegram.ml
+++ src/server/Controller_Telegram.ml
@@ -7,12 +7,25 @@ open Cohttp_lwt_unix
 open DiceLib
 open DiceParser
 
+(* ONLY FOR TESTING *)
+let to_string x =
+  match x with
+  | TelegramMessage.Roll _    -> "Roll Command Detected"
+  | TelegramMessage.Die _     -> "Die Command Detected"
+  | TelegramMessage.Game _    -> "Game Command Detected"
+
+let out x =
+  match x with
+  | Ok y    -> to_string y
+  | Error e -> e
+
 let parse_query query =
   "?" ^ query
   |> Uri.of_string
   |> (flip Uri.get_query_param) "message"
   |> Containers.Option.get_or ~default:"No param 'message' supplied.\n"
   |> TelegramMessage.parse
+  |> out
 
 let send_response body =
   Server.respond_string ~status:`OK ~body:body ()
